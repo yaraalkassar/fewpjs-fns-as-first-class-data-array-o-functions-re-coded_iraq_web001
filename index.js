a334e21dd88067e6023b6e90bfdc9cda12892c79
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-16 06:05:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
- function wakeDog(name="Xerox", breed="Border Collie") {
-    return(`Wake ${name} the ${breed}`);
-  }
-  function leashDog(name, breed) {
-    return(`Leash ${name} the ${breed}`);
-  }
-  function walkToPark(name, breed) {
-    return(`Walk to the park with ${name} the ${breed}`);
-  }
-  function throwFrisbee(name, breed) {
-    return(`Throw the frisbee for ${name} the ${breed}`);
-  }
-  function walkHome(name, breed) {
-    return(`Walk home with ${name} the ${breed}`);
-  }
+function wakeDog(name, breed) {
+   return(`Wake ${name} the ${breed}`);
+ }
+ function leashDog(name, breed) {
+   return(`Leash ${name} the ${breed}`);
+ }
+ function walkToPark(name, breed) {
+   return(`Walk to the park with ${name} the ${breed}`);
+ }
+ function throwFrisbee(name, breed) {
+   return(`Throw the frisbee for ${name} the ${breed}`);
+ }
+ function walkHome(name, breed) {
+   return(`Walk home with ${name} the ${breed}`);
+ }
 
-  function unleashDog(name, breed) {
-    return(`Unleash ${name} the ${breed}`);
-  }
+ function unleashDog(name, breed) {
+   return(`Unleash ${name} the ${breed}`);
+
+ }
+
 const routine=[wakeDog,
 leashDog,
 walkToPark,
@@ -25,5 +27,6 @@ walkHome,
 unleashDog];
 
 function exerciseDog (dogName, dogBreed) {
-  return routine.map(fn => fn(dogName, dogBreed))
-  }
+return routine.map(printArray => printArray(dogName, dogBreed));
+
+ }
